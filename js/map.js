87dd0389a5d7d237efe9350d8ab19a177ffe85b8
apunkari <70085350+apunkari@users.noreply.github.com>
2021-08-21 15:32:24 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -12,7 +12,7 @@ $(function() {
     fileType: 'csv',
     recordName: 'flu shot location',
     recordNamePlural: 'flu shot locations',
-    map_centroid: [41.85754, -87.66231],
+    map_centroid: [60.19480978722136, 24.917972222121577],
     defaultZoom:  11,
     defaultRadius: 1610,
     debug: false,
